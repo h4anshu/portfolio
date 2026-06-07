874a9f0473f2e41ef7094967a9c2f579dad9647a
Anshu <randomuse458@gmail.com>
2026-06-07 09:15:40 +0530

    Disable 3D character on mobile for performance - clean mobile hero layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useState, useEffect } from "react";
 import "./App.css";
 
 const CharacterModel = lazy(() => import("./components/Character"));
@@ -6,14 +6,24 @@ const MainContainer = lazy(() => import("./components/MainContainer"));
 import { LoadingProvider } from "./context/LoadingProvider";
 
 const App = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
       <LoadingProvider>
         <Suspense>
           <MainContainer>
-            <Suspense>
-              <CharacterModel />
-            </Suspense>
+            {!isMobile && (
+              <Suspense>
+                <CharacterModel />
+              </Suspense>
+            )}
           </MainContainer>
         </Suspense>
       </LoadingProvider>
